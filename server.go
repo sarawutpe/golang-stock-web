77e207d99b92eac6859da2c6c639d2f32dc16e14
Sarawut Pe <mr.sarawutpe@gmail.com>
2022-05-26 12:14:35 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"main/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/images", "./uploaded/images")
+	api.Setup(router)
+	router.Run()
+}
